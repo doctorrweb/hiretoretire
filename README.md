0eeee1bef06707b95cafb9b4ef232496835097ec
doctorrWeb <doctorrweb@gmail.com>
2020-11-25 19:04:29 +0100

    README.md updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# hiretoretire
+# Hire to Retire
+
+## Usage
+
+### Install Project Dependencies
+
+```````
+expo install
+```````
+
+## Start
+
+> Run te App
+
+``````
+expo start
+``````
+
+- version: 1.0.0
+- License: MIT
--- utils/getData.js
+++ utils/getData.js
@@ -1,23 +1,23 @@
-import axios from 'axios'
 
 const controller = new AbortController()
 const { signal } = controller
 
-const END_POINT_IMAGE = "https://chhr.afdb.org/wp-json/wp/v2/media"
-const END_POINT_POST = "https://chhr.afdb.org/wp-json/wp/v2/posts"
-const END_POINT_BENEFITS = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=102&_embed"
+const END_POINT_AFDB = "https://chhr.afdb.org/wp-json/wp/v2"
 
-const END_POINT_CHHR = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=106&_embed"
+const END_POINT_IMAGE = `${END_POINT_AFDB}/media`
 
-const END_POINT_SERVICES = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=105&_embed"
-const END_POINT_EVENTS = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=107&_embed"
-const END_POINT_INFOS = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=110&_embed"
+const END_POINT_POST = `${END_POINT_AFDB}/posts`
+const END_POINT_CAT = `${END_POINT_AFDB}/posts?categories`
 
-const END_POINT_FAQS = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=104&_embed"
-const END_POINT_GUIDANCES = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=108&_embed"
-const END_POINT_OTHERS = "https://chhr.afdb.org/wp-json/wp/v2/posts?categories=109&_embed"
+const END_POINT_SERVICES = `${END_POINT_AFDB}/${END_POINT_CAT}=105&_embed`
+const END_POINT_EVENTS = `${END_POINT_AFDB}/${END_POINT_CAT}=107&_embed`
+const END_POINT_INFOS = `${END_POINT_AFDB}/${END_POINT_CAT}=110&_embed`
+const END_POINT_FAQS = `${END_POINT_AFDB}/${END_POINT_CAT}=104&_embed`
+const END_POINT_GUIDANCES = `${END_POINT_AFDB}/${END_POINT_CAT}=108&_embed`
+const END_POINT_OTHERS = `${END_POINT_AFDB}/${END_POINT_CAT}=109&_embed`
 
-const END_POINT_AFDB = "https://chhr.afdb.org/wp-json/wp/v2/"
+const END_POINT_BENEFITS = `${END_POINT_AFDB}/${END_POINT_CAT}=102&_embed`
+const END_POINT_CHHR = `${END_POINT_AFDB}/${END_POINT_CAT}=106&_embed`
 
 
 export const getData = async (category) => {
@@ -56,52 +56,9 @@ export const getData = async (category) => {
 
     } catch (error) {
       console.log(error)
-      // Wait 2 seconds to abort both requests
-      // setTimeout(() => controller.abort(), 2000)
-      // controller.abort()
   }
 }
 
-// export const getApi = async () => {
-
-//   let uri = ''
-
-//   switch (category) {
-//     case 'services':
-//       uri = END_POINT_SERVICES
-//       break
-//     case 'events':
-//       uri = END_POINT_EVENTS
-//       break
-//     case 'faqs':
-//       uri = END_POINT_FAQS
-//       break
-//     case 'news':
-//       uri = END_POINT_INFOS
-//       break
-//     case 'guidances':
-//       uri = END_POINT_GUIDANCES
-//       break
-//     case 'otherDocs':
-//       uri = END_POINT_OTHERS
-//       break
-  
-//     default: uri = END_POINT
-//       break
-//   }
-
-//   try {
-
-//     axios.get(uri).then(res => {
-
-//     })
-
-//   } catch (error) {
-    
-//   }
-  
-
-// } 
 
 export async function getPost(postId) {
     try {
